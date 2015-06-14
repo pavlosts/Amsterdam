4c7d434dba2e4cb5014c6b94f4b8b557b92be216
pavlosts <pavlos_tsaou@hotmail.gr>
2015-06-14 18:00:28 +0300

    animation

--- animate.js
+++ animate.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-	var op = $('Menu').css('opacity');
-
+	$('.Name p').fadeIn(2000);
+	$('.Description p').fadeIn(2000);
+	$(".Menu ul").fadeIn(1800);
+	$(".Menu").animate({'top': '0px'}, 1000);
 	$(window).scroll(function(){
 		if($(window).scrollTop()>10){
-			/*$(".Menu").css( { "opacity": 1 - (($(this).scrollTop())/300) } );*/
-			$(".Menu").animate({'opacity': '0.1'})
-		}else{
-			$('.Menu').animate({'opacity': '0.9'})
+			$(".Menu").fadeTo(800, 0.1)
+		}
+		if($(window).scrollTop() == 0){
+			$('.Menu').fadeTo(500, 0.9)
 		}
 	});
 	$('.Menu').hover(function(){
-		op = $('Menu').css('opacity')
-		$('.Menu').animate({'opacity':'0.9'})
+		$('.Menu').fadeTo(500, 0.9)
 	},
 	function(){
-		if($('Menu').scrollTop() > 10){
-			$('.Menu').animate({'opacity': '0.1'})
+		if($(window).scrollTop() > 10){
+			$(".Menu").fadeTo(800, 0.1)
 		}
-	})
+	});
 });
