d60ffd1c469421a6abed591fda4d61e047923b86
pavlosts <pavlos_tsaou@hotmail.gr>
2015-06-14 20:56:54 +0300

    αλλαγη στο animation

--- animate.js
+++ animate.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
 	$('.Description p').fadeIn(2000);
 	$(".Menu ul").fadeIn(1800);
 	$(".Menu").animate({'top': '0px'}, 1000);
-	$(window).scroll(function(){
+	/*$(window).scroll(function(){
 		if($(window).scrollTop()>10){
 			$(".Menu").fadeTo(800, 0.1)
 		}
 		if($(window).scrollTop() == 0){
 			$('.Menu').fadeTo(500, 0.9)
 		}
-	});
+	},function(){
+		if($(window).scrollTop()>)
+	});*/
 	$('.Menu').hover(function(){
 		$('.Menu').fadeTo(500, 0.9)
 	},
 	function(){
-		if($(window).scrollTop() > 10){
-			$(".Menu").fadeTo(800, 0.1)
-		}
+		$(".Menu").fadeTo(800, 0.1)
 	});
 });
