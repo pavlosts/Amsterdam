6978e80748b1dee69eccbf5c81b7e02328ee2899
pavlosts <pavlos_tsaou@hotmail.gr>
2015-06-14 15:37:17 +0300

    εφτιαξα το animation

--- animate.js
+++ animate.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
+	var op = $('Menu').css('opacity');
+
+	$(window).scroll(function(){
+		if($(window).scrollTop()>1){
+			/*$(".Menu").css( { "opacity": 1 - (($(this).scrollTop())/300) } );*/
+			$(".Menu").animate({'opacity': '0.1'})
+		}else{
+			$('.Menu').animate({'opacity': '0.9'})
+		}
+	});
 	$('.Menu').hover(function(){
+		op = $('Menu').css('opacity')
 		$('.Menu').animate({'opacity':'0.9'})
 	},
-	function(){
-		$('.Menu').animate({'opacity':'0.1'})
-	});
+	function(op){
+		$('.Menu').animate({'opacity': '0.1'})
+	})
 });
